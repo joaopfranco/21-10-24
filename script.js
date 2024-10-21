6795a18fe3b5325866e27233ff2e99c4d1b98143
João Franco <143900584+joaopfranco@users.noreply.github.com>
2024-10-21 12:33:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// declaração de variáveis
+
+var pn = array[5, 0];
+var pp = array[5, 0];
+
+function cadpn() {
+    for (let i = 0; i <= 5; i++) {
+        pn[i] = parseFloat(prompt("Digite o nome do produto"))
+        console.log(pn);
+    }
+}
+
+function cadpp() {
+    for (let i = 0; i <= 5; i++) {
+        pn[i] = parseFloat(prompt("Digite o nome do produto"))
+        console.log(pn);
+    }
